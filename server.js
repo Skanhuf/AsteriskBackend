9645aadc4e780c71fd23199df445e193600e4345
Владислав Тимурович <apple@MacBook-Air-Vladislav.local>
2022-10-18 14:30:55 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+var cors = require('cors')
+app.use(cors())
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+const { Client } = require("pg")
+const dotenv = require("dotenv");
+const { addListener } = require("nodemon");
+dotenv.config()
+
+const client = new Client({
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.login,
+    password: process.env.password,
+    port: process.env.port
+})
+
+client.connect().then(() => {
+    app.get('/getData', async function (req, res, next) {
+        const data = await client.query('SELECT cdate,ctime,status,srcnum,dstnum,city,operator,cduration,qwaiting,messageid,recordingfile  FROM cdr')
+        res.json(data)
+
+    })
+})
